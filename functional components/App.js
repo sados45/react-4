514c22a4ec0c158ebf58dc8305f8e22fc4b90e6a
sados45 <sados45@gmail.com>
2022-06-28 14:43:23 +0200

    functional component

--- functional components/App.js
+++ functional components/App.js
@@ -1,6 +1,6 @@
 
 // import React, { Component } from 'react'
-
+// import Navbar from ".components/Navbar"
 // import './App.css';
 
 
@@ -9,31 +9,13 @@
 //  render() {
 //    return (
 //        <div className="container">
-//          <h4 className="header">App Component</h4>  {/*style = {{color:"red", fontSize:"40px", alignItem:"center", margin:0, padding:0}}*/} 
+//          <h4>App Component</h4> 
+//          <Navbar/>
 //        </div>
 //    )
 //  }
-// }
+//  }
 
 // export default App; 
-
-import React, { Component } from 'react';
-import Navbar from "./components/Navbar";
-import './App.css';
-
-
-class App extends Component {
-
- render() {
-   return (
-       <div className="container">
-      
-         <Navbar/>
-       </div>
-   )
- }
- }
-
-export default App; 
 
 //eger biz state yapisini hic kullanmayacaksak funtional componenti kullanacagiz. yani sadece props gönderilecekse functional components kullanilmasi mantiklidir. diger componenetimiz class base component.class base componenetimizde hem props hem de stateimiz olabilir. 
---
+++ hallo-app/src/components/Navbar.js
@@ -0,0 +1,25 @@
+// import React from "react";
+
+// const  Navbar = () => { //bu function oldugu icin yani class olmadigindan render a ihtiyac yok.sadece function Nabar () {} yazmak da yeterli. 
+
+//     return(
+//         <div>
+//             <h3>User App</h3>
+//         </div>
+//     )
+// }
+
+// export default Navbar;
+
+// Bu kismi rfc kisa yolu ile yaptik.
+import React from 'react'
+
+ function Navbar() {
+  return (
+    <div>
+
+        <h3>User App</h3>
+    </div>
+  )
+}
+export default Navbar;
